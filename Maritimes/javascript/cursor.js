3841147ff5da670151a4a96207028ae044adb905
Seamusboy <james@crowtreesstudio.com>
2019-11-18 13:03:39 -0500

    Further attempts to fix problem

--- Maritimes/javascript/cursor.js
+++ Maritimes/javascript/cursor.js
@@ -6,29 +6,6 @@ AFRAME.registerComponent('clickme', {
             lastIndex = (lastIndex + 1) % COLORS.length;
             this.setAttribute('material', 'color', COLORS[lastIndex]);
 //            console.log('I was clicked at: ', evt.detail.intersection.point);
-            
-/*************************************************/            
-            // Give permission to use Motoin & Orientation
-            DeviceMotionEvent.requestPermission()
-            .then(response => {
-              if (response == 'granted') {
-                window.addEventListener('devicemotion', (e) => {
-                  // do something with e
-                })
-              }
-            })
-            .catch(console.error)
-            
-            DeviceOrientationEvent.requestPermission()
-            .then(response => {
-              if (response == 'granted') {
-                window.addEventListener('deviceorientation', (e) => {
-                  // do something with e
-                })
-              }
-            })
-            .catch(console.error)
-/*************************************************/
         });
     }
 });
@@ -52,3 +29,32 @@ function hidePlay(){
     sound4.components.sound.playSound();
     
 }
+
+/*************************************************/            
+            // Give permission to use Motoin & Orientation
+window.onload = function(){
+    if(typeof DeviceMotionEvent.requestPermission === 'function'){
+        DeviceMotionEvent.requestPermission()
+        .then(response => {
+          if (response == 'granted') {
+            window.addEventListener('devicemotion', (e) => {
+              // do something with e - not necessary
+            })
+          }
+        })
+        .catch(console.error)
+    }
+    if(typeof DeviceOrientationEvent.requestPermission === 'function'){
+        DeviceOrientationEvent.requestPermission()
+        .then(response => {
+          if (response == 'granted') {
+            window.addEventListener('deviceorientation', (e) => {
+              // do something with e - not necessary
+            })
+          }
+        })
+        .catch(console.error)
+    }
+}
+            
+/*************************************************/
