f5a554ab9a0b8e9a45e347676d13f6bff5ace60a
Seamusboy <james@crowtreesstudio.com>
2020-03-17 16:10:19 -0400

    Mucking about
    Code seems too specific. Want to try and get it to work on identifying the element clicked on. From there can do other logical processing.

--- ARtap/events.js
+++ ARtap/events.js
@@ -2,15 +2,20 @@
 AFRAME.registerComponent('markerhandler', {
 
     init: function() {
-        const animatedMarker = document.querySelector("#animated-marker");
+        const animatedMarker = document.querySelector("#marker");
         const aEntity = document.querySelector("#animated-model");
 
         // every click, we make our model grow in size :)
-        animatedMarker.addEventListener('click', function(ev, target){
-            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+        animatedMarker.addEventListener('click', function(ev){
+            console.log("ev =", ev);
+            console.log("ev.detail.inter =", ev.detail.intersectedEl);
+            console.log("ID of object clicked =", ev.target.id);
+//            console.log("target = ", target);
+            const intersectedElement = ev.detail.intersectedEl;
+//            console.log(intersectedElement);
             if (aEntity && intersectedElement === aEntity) {
                 const scale = aEntity.getAttribute('scale');
-                Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
+                Object.keys(scale).forEach((key) => scale[key] = scale[key] + 0.1);
                 aEntity.setAttribute('scale', scale);
             }
         });
