da6fb9fccf05c4d4910394da131a496c899246b2
sheridanJames <11064115+CrowtreesStudio@users.noreply.github.com>
2022-06-09 09:22:47 -0400

    Level with Teleport
    Testing current state of tutorial level with teleport enabled.

---
+++ 8_Components_movement/components/mesh-shadows.js
@@ -0,0 +1,20 @@
+AFRAME.registerComponent('mesh-shadows', {
+    // built-in method
+    update:function(){
+        this.el.addEventListener('model-loaded', ()=>{
+            console.log("mesh check model loaded");
+            const el = this.el;
+            console.log("el to show object", el.object3D);
+            const obj = this.el.getObject3D('mesh');
+            obj.children.forEach((model)=>{
+                model.receiveShadow=true;
+                if(model.name === "ground"){
+                    model.castShadow=false;
+                }else{
+                    model.castShadow=true;
+                    model.material.shadowSide=1;
+                }
+            });
+        });
+    }
+});
---
+++ 8_Components_movement/readme.md
@@ -0,0 +1,2 @@
+Adding simple-navmesh-constraint.js component to use navmesh to constrain movement.
+Implement hand controls and teleport.
