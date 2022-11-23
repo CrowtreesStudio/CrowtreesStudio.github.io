6641b86c0088e318a3eeb961f44d47664bbdfb0a
sheridanJames <11064115+CrowtreesStudio@users.noreply.github.com>
2022-11-23 09:47:06 -0500

    See if this works
    Last upload grabbing not working

--- VR_basic_teleport_superhands/JS/code.js
+++ VR_basic_teleport_superhands/JS/code.js
@@ -20,7 +20,7 @@ AFRAME.registerComponent('contact-listener', {
     }
 });
 
-AFRAME.registerComponent('grabbed', {
+AFRAME.registerComponent('grabbingnow', {
     schema:{
         feedbackTXT:{type:'selector', default:'#feedback'},
     },
