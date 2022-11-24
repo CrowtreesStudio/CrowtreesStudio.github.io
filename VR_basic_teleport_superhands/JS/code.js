cf68282248c21eab58de7c8c23d49b5991cfd389
sheridanJames <11064115+CrowtreesStudio@users.noreply.github.com>
2022-11-24 09:50:26 -0500

    Targetting Grabbed Entity
    Working on getting the scoping correct.

--- VR_basic_teleport_superhands/JS/code.js
+++ VR_basic_teleport_superhands/JS/code.js
@@ -1,24 +1,3 @@
-AFRAME.registerComponent('contact-listener', {
-    init: function () {
-        var message = "Now it's ";
-        let object = this.el.id;
-        let color = '#9300ff';
-        
-        console.log("hello world", object);
-        
-        this.el.addEventListener('over', function(evt){
-            document.getElementById("text").setAttribute("value", message+object);
-            this.setAttribute('material', 'color', color);
-        });
-        
-//        this.el.addEventListener('click', function (evt) {
-//            lastIndex = (lastIndex + 1) % COLORS.length;
-//            this.setAttribute('material', 'color', COLORS[lastIndex]);
-//            console.log('I was clicked at: ', evt.detail.intersection.point);
-//            document.getElementById("text").setAttribute("value", message+COLORS[lastIndex]);
-//        });
-    }
-});
 AFRAME.registerComponent('pointer', {
     // built-in method
     init:function(){
@@ -67,8 +46,11 @@ AFRAME.registerComponent('grabbingtest', {
         feedbackTXT:{type:'selector', default:'#feedback'}
     },
     init: function(){
+        /* ******************************************************* */
+        /* Component to listen for controllers grabbing an entity  */
+        /* ******************************************************* */
         let data = this.data;
-        let message = "Version: 1.0.8";
+        let message = "Version: 1.0.9";
         const SET_COMP_PROPS = AFRAME.utils.entity.setComponentProperty;
         SET_COMP_PROPS(data.feedbackTXT, 'value', "Listening...");
         document.getElementById("text").innerHTML= message;
@@ -76,29 +58,32 @@ AFRAME.registerComponent('grabbingtest', {
     play: function() {
         let data = this.data;
         let el = this.el;
+
         el.addEventListener('grab-start', function(evt) {
             console.log(evt);
-            const SET_COMP_PROPS = AFRAME.utils.entity.setComponentProperty;
+            const SET_COMP_PROPS = AFRAME.utils.entity.setComponentProperty;// correct method to change attributes
             SET_COMP_PROPS(data.feedbackTXT, 'value', evt.detail.hand.id);
-            // SET_COMP_PROPS(data.feedbackTXT, 'value', "Submariner...");
         });
+
         el.addEventListener('grab-end', function(evt) {
             console.log(evt);
             const SET_COMP_PROPS = AFRAME.utils.entity.setComponentProperty;
             SET_COMP_PROPS(data.feedbackTXT, 'value', evt.detail.target.id);
-            // SET_COMP_PROPS(data.feedbackTXT, 'value', "Submariner Walkabout");
-            console.log("Components:", evt.detail.target.components);
 
-            SET_COMP_PROPS(evt.detail.target.object.el, 'color', '#'+(Math.random()*0xFFFFFF<<0).toString(16));// not working
-            // SET_COMP_PROPS(evt.detail.target.components.material, 'color', '#'+(Math.random()*0xFFFFFF<<0).toString(16));// not working
+            console.log("Components:", evt.detail.target.components);// 'target' is a detail object
 
-            evt.preventDefault();
+            SET_COMP_PROPS(evt.detail.target.object.el, 'color', '#'+(Math.random()*0xFFFFFF<<0).toString(16));// new
+            evt.preventDefault();// not sure what this does yet
         });
     }
   })
 
+
 AFRAME.registerComponent('controllisten', {
     init: function(){
+        /* ******************************************************* */
+        /* Component to use the controller joystick to turn around */
+        /* ******************************************************* */
         let el = this.el;// controller
         let id = el.id;
         let player = document.getElementById("cameraRig");
@@ -121,43 +106,3 @@ AFRAME.registerComponent('controllisten', {
         });
     }
 });
-
-//Example code for implementing the thumbstick to rotate the camera
-// found on A-frame Slack - credit SirFizX
-
-//CS1.MyPlayer.Lh.addEventListener('axismove', e => {
-//    if (CS1.Cam.isSweeping) return;
-//
-//    if (CS1.Rig && CS1.Rig.rotateInSteps) {
-//        if ((e.detail.axis[2] > .5) && !CS1.Rig.isRotating) {
-//            CS1.Rig.object3D.rotateY(-CS1.Rig.rotateStep)
-//            CS1.MyPlayer.object3D.rotateY(-CS1.Rig.rotateStep)
-//            CS1.Rig.isRotating = true
-//            setTimeout(e => {
-//                CS1.Rig.isRotating = false
-//            }, 1000)
-//        } else if ((e.detail.axis[2] < -.5) && !CS1.Rig.isRotating) {
-//            CS1.Rig.object3D.rotateY(CS1.Rig.rotateStep)
-//            CS1.MyPlayer.object3D.rotateY(CS1.Rig.rotateStep)
-//            CS1.Rig.isRotating = true
-//            setTimeout(e => {
-//                CS1.Rig.isRotating = false
-//            }, 1000)
-//        }
-//    } else {
-//        if (e.detail.axis[2] > .5) {
-//            CS1.Rig.object3D.rotateY(-CS1.MyPlayer.rotSpeed)
-//            CS1.MyPlayer.object3D.rotateY(-CS1.MyPlayer.rotSpeed)
-//        } else if (e.detail.axis[2] < -.5) {
-//            CS1.Rig.object3D.rotateY(CS1.MyPlayer.rotSpeed)
-//            CS1.MyPlayer.object3D.rotateY(CS1.MyPlayer.rotSpeed)
-//        }
-//    }
-//    
-//    if (e.detail.axis[3] > .5) {
-//        CS1.MyPlayer.object3D.translateZ(CS1.MyPlayer.speed)
-//    } else if (e.detail.axis[3] < -.5) {
-//        CS1.MyPlayer.object3D.translateZ(-CS1.MyPlayer.speed)
-//    }
-//    
-//})
