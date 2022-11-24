48cd25a498ae730889bf54ca2df0836519ccc703
sheridanJames <11064115+CrowtreesStudio@users.noreply.github.com>
2022-11-24 14:04:32 -0500

    1.1.4.2 - cursor saga con't

--- VR_basic_teleport_superhands/JS/code.js
+++ VR_basic_teleport_superhands/JS/code.js
@@ -18,8 +18,8 @@ AFRAME.registerComponent('scenemgr', {
             if(AFRAME.utils.device.checkHeadsetConnected() === true){
                 message = "Cursor has been hidden";
                 SET_COMP_PROPS(data.feedbackTXT, 'value', message);
-                console.log("cursor", cursor);
-                SET_COMP_PROPS(cursor.object.el , 'visible', false);
+                console.log("cursor", cursor.object);
+                SET_COMP_PROPS(cursor.object3D.el , 'visible', false);
             }else{
                 message = "It's Desktop or Mobile";
                 SET_COMP_PROPS(data.feedbackTXT, 'value', message);
@@ -31,7 +31,7 @@ AFRAME.registerComponent('scenemgr', {
 
     collectorMgmt: function(forCollection){
         const SET_COMP_PROPS = AFRAME.utils.entity.setComponentProperty;
-        console.log("Hello from collectorMgmt:", forCollection.object.el);
+        console.log("Hello from collectorMgmt:", forCollection.object);
         console.log("Hello from collectorMgmt object3D:", forCollection.object3D.el);
         // forCollection.object.el.setAttribute('class', 'not-clickable');
         // SET_COMP_PROPS(forCollection.object3D.el, 'visible', false);
@@ -82,10 +82,12 @@ AFRAME.registerComponent('grabbingtest', {
     },
     init: function(){
         let data = this.data;
-        let message = "Version: 1.1.4.1";
+        let message = "Version: 1.1.4.2";
         const SET_COMP_PROPS = AFRAME.utils.entity.setComponentProperty;
         SET_COMP_PROPS(data.feedbackTXT, 'value', "Listening...");
         document.getElementById("text").innerHTML= message;
+
+        console.log("scene locator:", sceneLocator);
     },
     play: function() {
         let data = this.data;
