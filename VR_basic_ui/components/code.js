15293945ca3c8eb8180808cda1d417af8d941a0e
sheridanJames <11064115+CrowtreesStudio@users.noreply.github.com>
2023-01-03 12:04:48 -0500

    Update code.js

--- VR_basic_ui/components/code.js
+++ VR_basic_ui/components/code.js
@@ -18,7 +18,7 @@ AFRAME.registerComponent('scenemgr', {
         let data = this.data;
         const SET_COMP_PROPS = AFRAME.utils.entity.setComponentProperty;
 
-        let message = "Version: 1.2.5";
+        let message = "Version: 1.2.5.2";
         document.getElementById("text").innerHTML= message;
         message = "listening...";
 
