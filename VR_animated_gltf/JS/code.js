ff5604b7c9dadcea7964d79deef5ba92b925ee77
sheridanJames <11064115+CrowtreesStudio@users.noreply.github.com>
2023-01-30 13:05:28 -0500

    Prepping for interaction
    Cleaned up scene and positioned characters for interaction.
    Navmesh not set up yet.

--- VR_animated_gltf/JS/code.js
+++ VR_animated_gltf/JS/code.js
@@ -1,7 +1,29 @@
 AFRAME.registerComponent('scenemgr', {
+    schema:{
+        activeCamRig:{type:'selector', default:'#cameraRig'}
+    },
     init: function(){
         let el = this.el;
         console.log("Contents of Scene:", el);
         console.log("Can I get the tag:",document.getElementsByTagName("img"));
+
+        window.addEventListener('load', evt=>{
+            // I'm leaving these here as a useful exploration
+            console.log("Device check:", AFRAME.utils.device);
+            console.log("WebXR enabled?:", AFRAME.utils.device.isWebXRAvailable);
+            console.log("Is it a Browser?:", AFRAME.utils.device.isBrowserEnvironment);
+            console.log("Is it a VR Display?:", AFRAME.utils.device.getVRDisplay());
+            console.log("Is it iOS?:", AFRAME.utils.device.isIOS());
+            console.log("Is it a HMD connected?:", AFRAME.utils.device.checkHeadsetConnected());
+        });
+
+        el.addEventListener('enter-vr', evt=>{
+            console.log("entering VR mode");
+            data.activeCamRig.setAttribute('movement-controls','enabled', false);
+        });
+        
+        el.addEventListener('exit-vr', evt=>{
+        });
+
     }
 });
