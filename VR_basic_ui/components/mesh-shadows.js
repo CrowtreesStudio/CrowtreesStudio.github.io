f77a4816546d09eb50dcfe6d3cb1b19b7c4279b2
sheridanJames <11064115+CrowtreesStudio@users.noreply.github.com>
2023-03-15 13:08:07 -0400

    Positioning of Characters
    Using Blender model, determined position of characters. Translation of coordinates from Blender to A-frame is so:
    blender[x:12.686, y:7.31, z: 0.379] = A-frame [x:12.686,  y:0.379, z:-7.31]
    Added cafe light, positioning and attribute adjustments done using A-frame inspector.

--- VR_basic_ui/components/mesh-shadows.js
+++ VR_basic_ui/components/mesh-shadows.js
@@ -13,11 +13,11 @@ AFRAME.registerComponent('mesh-shadows', {
             obj.children.forEach((model)=>{
                 if(model.children.length > 0){
                     console.log("There are children", model);
-                                model.children.forEach((node)=>{
-                                        node.castShadow=true;
-                                        node.receiveShadow=true;
-                                        // node.material.shadowSide=1;
-                                });
+                    model.children.forEach((node)=>{
+                        node.castShadow=true;
+                        node.receiveShadow=true;
+                        node.material.shadowSide=1;
+                    });
                 }else{
                     console.log("Simple shadow activation for", model.name);
                     model.castShadow=true;
