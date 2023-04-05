f57284f099ecd094786c8b12383b4fdcdc1df7e1
sheridanJames <11064115+CrowtreesStudio@users.noreply.github.com>
2023-04-05 13:55:16 -0400

    Added Glitch project
    the glitch project is an experiment and may define future implementation of projects if they're small.
    Small mods to mesh-shadows.
    Using simple-navmesh-constraint in place of aframe-extras
    Blink teleport added
    Removed unnecessary components.

--- VR_submariner_level/components/mesh-shadows.js
+++ VR_submariner_level/components/mesh-shadows.js
@@ -7,21 +7,18 @@ AFRAME.registerComponent('mesh-shadows', {
         this.el.addEventListener('model-loaded', ()=>{
             console.log("mesh check model loaded");
             const el = this.el;
-            console.log("el to show object", el.getObject3D('mesh'));
             const obj = this.el.getObject3D('mesh');
             obj.children.forEach((model)=>{
-                //model.receiveShadow=true;
                 model.material.shadowSide=1;
                 if(model.name === 'ground'){
                     model.castShadow=false;
                     model.receiveShadow=true;
                 }else if(model.name != 'coin'){
-                    model.castShadow=true;
-                    model.receiveShadow=true;
-                    // model.material.shadowSide=1;
+                    // model.castShadow=true;
+                    // model.receiveShadow=true;
                 }else{
                     model.castShadow=true;
-                    // model.material.shadowSide=1;
+                    model.receiveShadow=true;
                 }
             });
         });
